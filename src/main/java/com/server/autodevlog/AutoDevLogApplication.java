ceab76c398ab09e26bac93ae4435f5203ceccf2e
k0000k <locker50@naver.com>
2024-05-03 19:43:13 +0900

    init

---
+++ src/main/java/com/server/autodevlog/AutoDevLogApplication.java
@@ -0,0 +1,13 @@
+package com.server.autodevlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDevLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDevLogApplication.class, args);
+	}
+
+}
