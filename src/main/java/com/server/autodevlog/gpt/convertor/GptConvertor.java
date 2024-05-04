4b58fadb060d4d1a8f2dd989d6dfe9f57b3061b8
bell-person-ii <leeji88460@gmail.com>
2024-05-05 00:39:10 +0900

    주석 수정

--- src/main/java/com/server/autodevlog/gpt/convertor/GptConvertor.java
+++ src/main/java/com/server/autodevlog/gpt/convertor/GptConvertor.java
@@ -1,7 +1,8 @@
 package com.server.autodevlog.gpt.convertor;
 
 public class GptConvertor {
-    public static String deleteChangeLine(String target){
-        return target.replace("\n",""); // gpt api 응답 바디 내용중"\n" 삭제
+    // gpt api 응답 바디 내용중"\n" 삭제
+    public static String deleteChangeLine(String target) {
+        return target.replace("\n","");
     }
 }
