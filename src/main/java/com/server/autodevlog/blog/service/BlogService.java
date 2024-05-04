d21fa64840a254a3e7785f84db9472d1196f8b0b
k0000k <locker50@naver.com>
2024-05-04 20:39:21 +0900

    fix: extract method

--- src/main/java/com/server/autodevlog/blog/service/BlogService.java
+++ src/main/java/com/server/autodevlog/blog/service/BlogService.java
@@ -14,7 +14,7 @@ public class BlogService {
         String title = velogPostDto.getTitle();
         String body = velogPostDto.getBody();
 
-        String query = "{\"operationName\":\"WritePost\",\"variables\":{\"title\":\"" + title + "\",\"body\":\"" + body + "\",\"tags\":[],\"is_markdown\":true,\"is_temp\":false,\"is_private\":false,\"url_slug\":\"" + title + "\",\"thumbnail\":null,\"meta\":{\"short_description\":\"" + body + "\"},\"series_id\":null,\"token\":null},\"query\":\"mutation WritePost($title: String, $body: String, $tags: [String], $is_markdown: Boolean, $is_temp: Boolean, $is_private: Boolean, $url_slug: String, $thumbnail: String, $meta: JSON, $series_id: ID, $token: String) {\\n  writePost(title: $title, body: $body, tags: $tags, is_markdown: $is_markdown, is_temp: $is_temp, is_private: $is_private, url_slug: $url_slug, thumbnail: $thumbnail, meta: $meta, series_id: $series_id, token: $token) {\\n    id\\n    user {\\n      id\\n      username\\n      __typename\\n    }\\n    url_slug\\n    __typename\\n  }\\n}\\n\"}";
+        String query = buildVelogQuery(title, body);
 
         WebClient webClient = WebClient.builder()
                 .baseUrl("https://v2.velog.io")
@@ -28,5 +28,16 @@ public class BlogService {
                 .retrieve()
                 .toEntity(String.class)
                 .block();
+    }
+
+    private String buildVelogQuery(String title, String body) {
+        return "{\"operationName\":\"WritePost\",\"variables\"" +
+                ":{\"title\":\"" + title + "\",\"body\":\"" + body
+                + "\",\"tags\":[],\"is_markdown\":true,\"is_temp\":false,\"is_private\":false,\"url_slug\":\""
+                + title + "\",\"thumbnail\":null,\"meta\":{\"short_description\":\"" + body + "\"}," +
+                "\"series_id\":null,\"token\":null},\"query\":\"mutation " +
+                "WritePost($title: String, $body: String, $tags: [String], $is_markdown: Boolean, $is_temp: Boolean, $is_private: Boolean, $url_slug: String, $thumbnail: String, $meta: JSON, $series_id: ID, $token: String) " +
+                "{\\n  writePost(title: $title, body: $body, tags: $tags, is_markdown: $is_markdown, is_temp: $is_temp, is_private: $is_private, url_slug: $url_slug, thumbnail: $thumbnail, meta: $meta, series_id: $series_id, token: $token) " +
+                "{\\n    id\\n    user {\\n      id\\n      username\\n      __typename\\n    }\\n    url_slug\\n    __typename\\n  }\\n}\\n\"}";
     }
 }
