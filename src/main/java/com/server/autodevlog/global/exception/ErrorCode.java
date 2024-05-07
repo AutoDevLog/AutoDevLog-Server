bd76633ac86cf0878a75a68ba82f26f97cef0278
k0000k <locker50@naver.com>
2024-05-07 14:01:17 +0900

    fix: err code

--- src/main/java/com/server/autodevlog/global/exception/ErrorCode.java
+++ src/main/java/com/server/autodevlog/global/exception/ErrorCode.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     GPT_API_ERROR(HttpStatus.SERVICE_UNAVAILABLE,"GPT-503","GPT API가 정상적으로 응답하지 않습니다."),
-    VELOG_RESPONSE_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "VELOG-400", "Velog 서버가 정상적으로 응답하지 않습니다."),
-    VELOG_POSTING_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "VELOG-500", "Velog 포스팅에 실패했습니다.");
+    VELOG_RESPONSE_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "VELOG-503", "Velog 서버가 정상적으로 응답하지 않습니다."),
+    VELOG_POSTING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "VELOG-500", "Velog 포스팅에 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
